1e6a6cf76ffeab3e6f19128e88de653a305610e0
jwucong <jwucong@gmail.com>
2019-04-25 18:28:48 +0800

    222

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# matrix
+# Matrix
+
+**1. Matrix(rows, items)**
+   ```javascript
+   const matrix = new Matrix(2, [1, 3, 0, 1, 2, 0, 1, 1])
+   // or
+   const matrix = Matrix(2, [1, 3, 0, 1, 2, 0, 1, 1])
+   
+   ```
--- matrix.js
+++ matrix.js
@@ -1,25 +1,39 @@
-// Matrix(2,  2,3,4,5,5,6,7)
-function Matrix(rows = 1, items = []) {
+
+function Matrix(rows, items) {
   if (rows instanceof Matrix) {
     return rows
   }
   if (!(this instanceof Matrix)) {
     return new Matrix(rows, items)
   }
-  if (Array.isArray(rows)) {
-    items = rows
-    rows = 1
+  if (_isMatrixLike(rows)) {
+    const obj = _parseMatrixLike(rows)
+    rows = obj.rows
+    items = obj.items
   }
-  const size = items.length
-  const n = parseInt(rows, 10)
-  const m = n < 1 ? 1 : n > size ? size : n
-  const columns = Math.floor(size / m)
+  const list = _is(items, 'array') ? items : []
+  const size = list.length
+  let m = parseInt(rows, 10) || 0
+  let n = 0
+  m = m > size ? size : m
+  n = n > size ? size : n
+  if (/^\d+$/.test(m) && m > 0) {
+    n = Math.floor(size / rows)
+    if (m < 2 && n < 2) {
+      throw Error('Matrix requires at least two elements.')
+    }
+  } else {
+    if (size < 4) {
+      throw Error('A square matrix requires at least four elements.')
+    }
+    m = n = Math.floor(Math.sqrt(size))
+  }
   let i = 0, j = 0;
-  for (; i < size, j < m; i += columns, j++) {
-    this[j] = items.slice(i, i + columns)
+  for (; i < size, j < m; i += n, j++) {
+    this[j] = items.slice(i, i + n)
   }
-  this.rows = j
-  this.columns = columns
+  this.rows = m
+  this.columns = n
   this.isMatrix = true
 }
 
@@ -27,40 +41,76 @@ Matrix.prototype.get = function (row, column) {
   return this[row][column]
 }
 
+// 加法
+Matrix.prototype.addition = function (matrix) {
+  const A = this
+  const B = new Matrix(matrix)
+  const m = A.rows
+  const n = A.columns
+  if (m !== B.rows || n !== B.columns) {
+    throw Error('A matrix and B matrix are not homomorphic matrices')
+  }
+  let result = []
+  for (let i = 0; i < m; i++) {
+    result.push(A[i].map((item, j) => item + B[i][j]))
+  }
+  return new Matrix(result)
+}
+
+// 减法
+Matrix.prototype.subtraction = function (matrix) {
+  return this.addition(Matrix(matrix).multiplication(-1))
+}
+
 // 乘法
 Matrix.prototype.multiplication = function (matrix) {
-  const rows = this.rows
-  const cols = this.columns
-  let items = []
-  if (matrix instanceof Matrix) {
-    if (cols !== matrix.rows) {
-      throw Error('参数矩阵行数与当前矩阵列数不相等')
+  // A * B => {A: m * n, B: n * p}
+  const num = parseFloat(matrix)
+  const A = this
+  const m = A.rows
+  const n = A.columns
+  let result = []
+
+  if (!Number.isNaN(num)) {
+    for (let i = 0; i < m; i++) {
+      result.push(A[i].map(item => num * item))
     }
-    for (let i = 0; i < rows; i++) {
-      const row = []
-      for (let j = 0; j < cols; j++) {
-        const col = this[i][j] * matrix[j][i]
-        // TODO 矩阵乘法
-        row.push(col)
-      }
-      items = items.concat(row)
-    }
-    return new Matrix(rows, items)
-  } else {
-    const n = parseFloat(matrix)
-    if (!Number.isNaN(n)) {
-      for (let i = 0; i < rows; i++) {
-        const row = this[i].map(item => n * item)
-        items = items.concat(row)
-      }
-      return new Matrix(rows, items)
-    }
+    return new Matrix(result)
   }
+
+  const B = new Matrix(matrix)
+  if (n !== B.rows) {
+    throw Error('The B matrix rows is not equal to the A matrix columns.')
+  }
+  const p = B.columns
+  for (let i = 0; i < m; i++) {
+    let row = []
+    for (let j = 0; j < p; j++) {
+      let col = 0
+      for (let k = 0; k < n; k++) {
+        col += A[i][k] * B[k][j]
+      }
+      row.push(col)
+    }
+    result.push(row)
+  }
+  return new Matrix(result)
 }
 
 // 矩阵转置
 Matrix.prototype.translation = function () {
-
+  const A = this
+  const m = A.rows
+  const n = A.columns
+  let result = []
+  for (let i = 0; i < n; i++) {
+    let row = []
+    for (let j = 0; j < m; j++) {
+      row.push(A[j][i])
+    }
+    result.push(row)
+  }
+  return new Matrix(result)
 }
 
 function _is(v, t) {
@@ -68,49 +118,95 @@ function _is(v, t) {
   return typeof t === 'string' ? c === t.toLowerCase() : c
 }
 
-function _hasOwnProperties(obj, props) {
-  return props.every(prop => obj.hasOwnProperty(prop))
-}
-
 function _isMatrixLike(value) {
-  // TODO 类矩阵对象
-  const type = _is(value)
-  console.log('type: ', type)
-  if (!(type === 'object' || type === 'array')) {
-    return false
-  }
   if (_is(value, 'object')) {
-    let size = 0
-    const rows = Object.keys(value).filter(key => /^\d+$/.test(key))
-    console.log('rows: ', rows)
-    if (!rows.length) {
+    const rows = _getSerialKeys(value)
+    const size = rows.length
+    if (!size) {
       return false
     }
-    const flag = rows.every(row => {
-      const a = value[row]
-      if (!_is(a, 'array')) {
+    for (let i = 0; i < size; i++) {
+      const a = value[rows[i]]
+      const e = _getArrayDepth(a) === 1
+      if (!e || a.length !== value[0].length) {
         return false
       }
-      if (!size) {
-        size = value[rows[0]].length
-      }
-      const f = a.every(item => {
-        const n = parseFloat(item)
-        return _is(n, 'number') && !Number.isNaN(n)
-      })
-      return a.length === size && f
-    })
-    console.log(rows)
-    console.log(flag)
-    return flag
+    }
+    return true
   }
-  
+  if (_is(value, 'array')) {
+    const d = _getArrayDepth(value)
+    const size = value.length
+    if (d === 1) {
+      return size > 0
+    }
+    if (d === 2) {
+      for (let i = 0; i < size; i++) {
+        const a = value[i]
+        const e = _getArrayDepth(a) === 1
+        if (!e || a.length !== value[0].length) {
+          return false
+        }
+      }
+      return true
+    }
+    return false
+  }
+  return false
 }
 
-const m1 = Matrix(2, [1, 0, 2, -1, 3, 1])
-const m2 = Matrix(3, [3, 1, 2, 1, 1, 0])
+function _getArrayDepth(array) {
+  const counter = (a, n) => {
+    if (!_is(a, 'array')) {
+      return n
+    }
+    n++
+    const items = a.map(item => counter(item, n))
+    return items.length ? Math.max.apply(null, items) : n
+  }
+  return counter(array, 0)
+}
 
-console.log(m1)
-console.log(m2)
-console.log(m1.multiplication(m2))
-// console.log(_isMatrixLike(m1))
+function _getSerialKeys(obj) {
+  const reg = /^\d+$/
+  const keys = Object.keys(obj).filter(key => reg.test(key))
+  keys.sort((a, b) => a - b)
+  if (keys[0] !== '0') {
+    return []
+  }
+  let result = [keys[0]]
+  for (let i = 1; i < keys.length; i++) {
+    const key = keys[i]
+    if (key - result[result.length - 1] !== 1) {
+      return result
+    }
+    result.push(key)
+  }
+  return result
+}
+
+function _parseMatrixLike(matrixLike) {
+  let rows = 0, items = []
+  if (_is(matrixLike, 'array')) {
+    const one = _getArrayDepth(matrixLike) === 1
+    rows = one ? 0 : matrixLike.length
+    items = [].concat.apply(items, matrixLike)
+  } else {
+    const keys = _getSerialKeys(matrixLike)
+    const array = keys.map(key => matrixLike[key])
+    rows = keys.length
+    items = [].concat.apply(items, array)
+  }
+  return {rows, items}
+}
+
+const m1 = Matrix(2, [1, 3, 1, 1, 0, 0])
+const m2 = Matrix(2, [0, 0, 5, 7, 5, 0])
+const m3 = Matrix(2, [1, 0, 2, -1, 3, 1])
+const m4 = Matrix(3, [3, 1, 2, 1, 1, 0])
+const m5 = Matrix(2, [1, 2, 3, 0, -6, 7])
+
+console.log(m1.addition(m2))
+console.log(m1.subtraction(m2))
+console.log(m3.multiplication(m4))
+console.log(m5.translation())
